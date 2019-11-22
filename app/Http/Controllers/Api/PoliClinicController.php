b6358e2cd6f3487d55fafd4ec7957f199487adc2
Bill Tanthowi Jauhari <bill.tanthowi.j@gmail.com>
2019-11-22 16:18:43 +0700

    add documentation api for poli clinic

--- app/Http/Controllers/Api/PoliClinicController.php
+++ app/Http/Controllers/Api/PoliClinicController.php
@@ -11,6 +11,15 @@ use App\Http\Controllers\Controller;
 class PoliClinicController extends Controller
 {
     /**
+    @OA\Get(
+    path="/api/v1/poli/{hospital_id}",
+    tags={"Poli Clinic"},
+    summary="Get Policlinic list by query",
+    operationId="profile",
+
+    @OA\Response(response="default", description="successful operation")
+    )
+
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response

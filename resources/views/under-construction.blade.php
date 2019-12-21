2771938d1ac4666e58758353dd060c154d0d4ab3
ridolud <faridho.lutfi@gmail.com>
2019-12-22 01:05:17 +0700

    init landing page

---
+++ resources/views/under-construction.blade.php
@@ -0,0 +1,168 @@
+<!-- get_header('Page Name','Title')-->
+<!doctype html>
+<html class="no-js" lang="zxx">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Antridoc - Coming Soon</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700%7CRoboto:400,500,700,900%7CPlayfair+Display:400,700,700i,900,900i%7CWork+Sans:400,500,600,700" rel="stylesheet">
+
+        <!-- signatra-font -->
+        <link rel="stylesheet" href="assets/css/signatra-font.css">
+
+        <link rel="icon" type="image/png" href="favicon.ico">
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/animate.css">
+        <link rel="stylesheet" href="assets/css/iconfont.css">
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+        <link rel="stylesheet" href="assets/css/swiper.min.css">
+        <link rel="stylesheet" href="assets/css/rev-settings.css">
+
+        <!--For Plugins external css-->
+        <link rel="stylesheet" href="assets/css/plugins.css" />
+
+        <!--Theme custom css -->
+        <link rel="stylesheet" href="assets/css/style.css">
+
+        <!--Theme Responsive css-->
+        <link rel="stylesheet" href="assets/css/responsive.css" />
+    </head>
+    <body>
+    <!--[if lt IE 10]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+    <!-- prelaoder -->
+    <!-- <div id="preloader">
+    <div class="preloader-wrapper">
+        <div class="spinner"></div>
+    </div>
+    <div class="preloader-cancel-btn">
+        <a href="#" class="btn btn-secondary prelaoder-btn">Cancel Preloader</a>
+    </div>
+</div> -->
+    <!-- END prelaoder -->
+<div class="header-transparent nav-sticky nav-light">
+    <!-- header section -->
+    <<!-- div class="off-canvas-menu-area">
+    <div class="container">
+        <div class="offset-menu-wraper clearfix">
+            <div class="float-left">
+                <a href="#" class="offcanvas-nav-brand">
+                    <img src="assets/images/logo-5.png" alt="">
+                </a>
+            </div>
+            <div class="float-right">
+                <div class="offsetmenu-btn humburger fullscreen_menu_tigger clearfix">
+                    <div class="humburger-icons">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <!-- <div class="offcanvas-menu-wraper clearfix">
+        <div class="menu-container">
+            <ul class="offcanvas-menu single-page-menu">
+                <li><a href="#homes">HOME</a></li>
+                <li><a href="#services">SERVICE</a></li>
+                <li><a href="#testimonial">TESTIMONIAL</a></li>
+            </ul>
+        </div>
+        <div class="offcanvas-content-wraper">
+            <div class="offcanvas-content">
+                <h2 class="offcanvas-title">GET IN TOUCH</h2>
+                <ul class="offcanvas-list-widget">
+                    <li>
+                        <p><i class="icon icon-map-marker2"></i><a href="https://maps.google.com/maps?q=221B+Baker+Street,+London,+United+Kingdom&amp;hl=en&amp;t=v&amp;hnear=221B+Baker+St,+London+NW1+6XE,+United+Kingdom" target="_blank">179 MADISON ST. OLIVER AV. <br> NEW YORK, NY-12548</a></p>
+                    </li>
+                    <li>
+                        <p><i class="icon icon-phone-call3"></i><a href="tel:+1%20(254)%20854-6987">+1 (254) 854-6987</a></p>
+                    </li>
+                    <li>
+                        <p><i class="icon icon-envelope4"></i><a href="mailto:info@example.com">INFO@EXAMPLE.COM</a></p>
+                    </li>
+                </ul>
+                <h2 class="offcanvas-title">GET SOCIAL TOGETHER</h2>
+                <ul class="social-list version-4">
+                    <li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#" class="instagram"><i class="fa fa-instagram"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div> -->
+</div>    <!-- End header section -->
+</div>
+
+<!-- agency banner section -->
+<section class="xs-banner agency-banner6" id="homes" data-scrollax-parent="true">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 align-self-center">
+                <div class="agency-banner-content banner-style6">
+                    <h2 class="banner-title">Under <br><span class="title-underline">Construction </span></h2>
+                    <p>Our website is currently undergoing schedule maintenance. We should be back shortly.</p>
+                    <!-- <div class="banner-video-popups">
+                        <a href="#" class="xs-video-popup">
+                            <i class="icon icon-play"></i>
+                        </a>
+                        <span>Take a Video Tour</span>
+                    </div> -->
+                </div><!-- .agency-banner-content END -->
+            </div>
+            <div class="col-lg-5">
+                <div class="agency-banner-img">
+                    <img src="assets/images/welcome/agency_welcome_3.png" data-scrollax="properties: { translateY: '50%' }" alt="">
+                </div><!-- .agency-banner-img END -->
+            </div>
+        </div>
+    </div><!-- .container END -->
+    <div class="banner-icon-bg">
+        <img src="assets/images/welcome/welcome-icon-bg-3.png" alt="">
+    </div>
+</section><!-- end agency banner section -->
+        <script src="assets/js/jquery-3.2.1.min.js"></script>
+        <script src="assets/js/plugins.js"></script>
+        <script src="assets/js/Popper.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.magnific-popup.min.js"></script>
+        <script src="assets/js/owl.carousel.min.js"></script>
+        <script src="assets/js/scrollax.js"></script>
+        <script src="assets/js/jquery.ajaxchimp.min.js"></script>
+        <script src="assets/js/jquery.waypoints.min.js"></script>
+        <script src="assets/js/isotope.pkgd.min.js"></script>
+        <script src="assets/js/swiper.min.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyDeZubzJTQgtjreqsdaGMGXxaxP-pv6pSk"></script>
+        <script src="assets/js/jquery.easypiechart.min.js"></script>
+        <script src="assets/js/delighters.js"></script>
+        <script src="assets/js/typed.js"></script>
+        <script src="assets/js/jquery.parallax.js"></script>
+        <script src="assets/js/jquery.themepunch.tools.min.js"></script>
+        <script src="assets/js/jquery.themepunch.revolution.min.js"></script>
+        <script src="assets/js/extensions/revolution.extension.actions.min.js"></script>
+        <script src="assets/js/extensions/revolution.extension.carousel.min.js"></script>
+        <script src="assets/js/extensions/revolution.extension.kenburn.min.js"></script>
+        <script src="assets/js/extensions/revolution.extension.layeranimation.min.js"></script>
+        <script src="assets/js/extensions/revolution.extension.migration.min.js"></script>
+        <script src="assets/js/extensions/revolution.extension.navigation.min.js"></script>
+        <script src="assets/js/extensions/revolution.extension.parallax.min.js"></script>
+        <script src="assets/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <script src="assets/js/extensions/revolution.extension.video.min.js"></script>
+        <script src="assets/js/skrollr.min.js"></script>
+        <script src="assets/js/shuffle-letters.js"></script>
+        <script src="assets/js/main.js"></script>       <!-- End js file -->
+    </body>
+</html>

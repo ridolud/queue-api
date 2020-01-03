65f4f20bbbf54b288bdd4944834173af3546b54c
ridolud <faridho.lutfi@gmail.com>
2020-01-03 19:25:58 +0700

    update view email verification warding

--- resources/views/verified-email.blade.php
+++ resources/views/verified-email.blade.php
@@ -1,7 +1,101 @@
-your email has been verified!
-<!--
-/**
-* TODO: add associate code for deep link needed
-*
-*/
--->
+<!-- get_header('Page Name','Title')-->
+<!doctype html>
+<html class="no-js" lang="zxx">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Antridoc - Email Verification</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700%7CRoboto:400,500,700,900%7CPlayfair+Display:400,700,700i,900,900i%7CWork+Sans:400,500,600,700" rel="stylesheet">
+
+        <!-- signatra-font -->
+        <link rel="stylesheet" href="{{ asset('assets/css/signatra-font.css') }}">
+
+        <link rel="icon" type="image/png" href="favicon.ico">
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
+
+        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/iconfont.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/swiper.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/rev-settings.css') }}">
+
+        <!--For Plugins external css-->
+        <link rel="stylesheet" href="{{ asset('assets/css/plugins.css') }}"/>
+
+        <!--Theme custom css -->
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+        <!--Theme Responsive css-->
+        <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}"/>
+    </head>
+    <body>
+    <!--[if lt IE 10]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+    <!-- prelaoder -->
+    <!-- <div id="preloader">
+    <div class="preloader-wrapper">
+        <div class="spinner"></div>
+    </div>
+    <div class="preloader-cancel-btn">
+        <a href="#" class="btn btn-secondary prelaoder-btn">Cancel Preloader</a>
+    </div>
+</div> -->
+    <!-- END prelaoder -->
+
+<section class="xs-section-padding agency_callto_action2" data-scrollax-parent="true">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 mx-auto">
+                <div class="agency-section-title text-center">
+                    <h3 class="sub-title">Verification</h3>
+                    <h4 class="main-title">Email has ben <em>verified</em></h4>
+                    <p>Tanks for register us, click bellow button to open app</p>
+                    <span class="watermark-title" data-scrollax="properties: { translateY: '-250px' }" style="transform: translateZ(0px) translateY(135.15509601181685px);">VEREFIED</span>
+                    <a href="antridoc://emailverification" class="btn btn-primary">Open App</a>
+                </div>
+            </div>
+        </div><!-- .row END -->
+    </div><!-- .container END -->
+</section>
+
+        <script src="{{ asset('assets/js/jquery-3.2.1.min.js') }}"></script>
+        <script src="{{ asset('assets/js/plugins.js') }}"></script>
+        <script src="{{ asset('assets/js/Popper.js') }}"></script>
+        <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+        <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+        <script src="{{ asset('assets/js/scrollax.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.ajaxchimp.min.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.waypoints.min.js') }}"></script>
+        <script src="{{ asset('assets/js/isotope.pkgd.min.js') }}"></script>
+        <script src="{{ asset('assets/js/swiper.min.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.easypiechart.min.js') }}"></script>
+        <script src="{{ asset('assets/js/delighters.js') }}"></script>
+        <script src="{{ asset('assets/js/typed.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.parallax.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.themepunch.tools.min.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.themepunch.revolution.min.js') }}"></script>
+        <script src="{{ asset('assets/js/extensions/revolution.extension.actions.min.js') }}"></script>
+        <script src="{{ asset('assets/js/extensions/revolution.extension.carousel.min.js') }}"></script>
+        <script src="{{ asset('assets/js/extensions/revolution.extension.kenburn.min.js') }}"></script>
+        <script src="{{ asset('assets/js/extensions/revolution.extension.layeranimation.min.js') }}"></script>
+        <script src="{{ asset('assets/js/extensions/revolution.extension.migration.min.js') }}"></script>
+        <script src="{{ asset('assets/js/extensions/revolution.extension.navigation.min.js') }}"></script>
+        <script src="{{ asset('assets/js/extensions/revolution.extension.parallax.min.js') }}"></script>
+        <script src="{{ asset('assets/js/extensions/revolution.extension.slideanims.min.js') }}"></script>
+        <script src="{{ asset('assets/js/extensions/revolution.extension.video.min.js') }}"></script>
+        <script src="{{ asset('assets/js/skrollr.min.js') }}"></script>
+        <script src="{{ asset('assets/js/shuffle-letters.js') }}"></script>
+        <script src="{{ asset('assets/js/main.js') }}"></script>       <!-- End js file -->
+    </body>
+</html>

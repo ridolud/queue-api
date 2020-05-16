ff048ca2dd851eb7f5e3a803a3c5304001262d95
ridolud <faridho.lutfi@gmail.com>
2020-05-17 03:08:07 +0700

    email form name & email change

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}">
-            {{ $slot }}
+            <img src="{{ url('assets/antridoc/logo.png') }}" alt="antridoc-logo" style="height: 60px; width: auto;">
         </a>
     </td>
 </tr>
